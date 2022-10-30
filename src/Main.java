ca60be8cf18cc6fa67ad801e88b9d78d48594673
KSVarlamov <varlamov.ks@mail.ru>
2022-10-30 15:41:57 +0300

    Разделил упаковку и удаление файлов Вынес путь до основной директории в константу

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,33 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Main {
+    private static final String SAVE_PATH = "D:\\__Games\\savegames\\";
     public static void main(String[] args) {
         Map<String, GameProgress> saves = new HashMap<>();
-        saves.put("D:\\Games\\savegames\\save1.dat", new GameProgress(100, 1, 10, 1000.0));
-        saves.put("D:\\Games\\savegames\\save2.dat", new GameProgress(80, 4, 12, 19531.2));
-        saves.put("D:\\Games\\savegames\\save3.dat", new GameProgress(50, 8, 99, 65465465.2));
+        saves.put(SAVE_PATH + "save1.dat", new GameProgress(100, 1, 10, 1000.0));
+        saves.put(SAVE_PATH + "save2.dat", new GameProgress(80, 4, 12, 19531.2));
+        saves.put(SAVE_PATH + "save3.dat", new GameProgress(50, 8, 99, 65465465.2));
 
         for (String path : saves.keySet()) {
             saveGame(path, saves.get(path));
         }
-        zipFiles("D:\\Games\\savegames\\save.zip", saves.keySet().stream().toList());
+        if (zipFiles(SAVE_PATH + "save.zip", new ArrayList<>(saves.keySet()))) {
+            deleteFiles(new ArrayList<>(saves.keySet()));
+        }
+    }
+
+    private static void deleteFiles(List<String> paths) {
+        for (String path : paths) {
+            File f = new File(path);
+            f.delete();
+        }
     }
 
     public static void saveGame(String path, GameProgress gp) {
@@ -32,7 +44,7 @@ public class Main {
         }
     }
 
-    public static void zipFiles(String file, List<String> paths) {
+    public static boolean zipFiles(String file, List<String> paths) {
         try {
             ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(file));
             for (String path : paths) {
@@ -46,11 +58,11 @@ public class Main {
                 zos.write(buff);
                 zos.closeEntry();
                 fis.close();
-                f.delete();
             }
             zos.finish();
+            return true;
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            return  false;
         }
     }
 }
